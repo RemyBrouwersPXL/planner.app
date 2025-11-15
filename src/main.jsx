1c3c2f4269d578e7f338b85fbe13dc5b1fe8dfa3
RemyBrouwersPXL <remy.brouwers@student.pxl.be>
2025-11-15 19:01:31 +0100

    fsdgfsd

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Login from "./components/Login.jsx";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 serviceWorkerRegistration.register();
@@ -8,6 +9,7 @@ serviceWorkerRegistration.register();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Login />
     <App />
   </React.StrictMode>
 );
